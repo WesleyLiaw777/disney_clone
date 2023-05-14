579e4785689a1e6e85fffba68365836c4d5fbcf4
WesleyLiaw <wesleyliaw777@gmail.com>
2023-05-14 15:40:29 -0500

    tried firebase adapter

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+// import FirebaseAdapter from '@next-auth/firebase-adapter'
+// import { db } from "@/firebase"
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -9,6 +11,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
+  // adapter: FirebaseAdapter(db),
 }
 
 export default NextAuth(authOptions)
